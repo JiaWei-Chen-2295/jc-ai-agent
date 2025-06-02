267056991f259ce2698d662500f6a2f4fa89ad95
JiaWei-Chen-2295 <1601020332@qq.com>
2025-06-02 23:09:44 +0800

    feat(rag): 添加多查询扩展功能
    - 新增 MultiQueryExpanderTest 类，用于测试多查询扩展功能
    - 实现 MyMultiQueryExpander 类，提供多查询扩展功能
    - 使用 Spring Boot 和 Spring AI 相关库，集成聊天模型进行查询扩展

---
+++ src/main/java/fun/javierchen/jcaiagentbackend/rag/MyMultiQueryExpander.java
@@ -0,0 +1,32 @@
+package fun.javierchen.jcaiagentbackend.rag;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.rag.Query;
+import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 多查询扩展
+ */
+@Component
+public class MyMultiQueryExpander {
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    public List<Query> expand(Query singleQuery) {
+        MultiQueryExpander multiQueryExpander = MultiQueryExpander.builder()
+                .chatClientBuilder(ChatClient.builder(dashscopeChatModel))
+                // 节省资源
+                .numberOfQueries(5)
+                // 保留原始语义
+                .includeOriginal(true)
+                .build();
+        return multiQueryExpander.expand(singleQuery);
+    }
+
+}
---
+++ src/test/java/fun/javierchen/jcaiagentbackend/rag/MultiQueryExpanderTest.java
@@ -0,0 +1,23 @@
+package fun.javierchen.jcaiagentbackend.rag;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.rag.Query;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class MultiQueryExpanderTest {
+
+    @Resource
+    private MyMultiQueryExpander multiQueryExpander;
+
+    @Test
+    public void testMultiQueryExpander() {
+        List<Query> expandQueryList = multiQueryExpander.expand(new Query("嘛是 Java ，俺爱 Java 。"));
+        Assertions.assertNotNull(expandQueryList);
+    }
+
+}
